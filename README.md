f12401064efb0bf4efa831b45a65436936153a5a
Alex Balo <69122071+todplaz@users.noreply.github.com>
2020-10-15 14:23:34 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+Il y a deux joueurs; l’un joue avec le signe X et l’autre avec le signe O.
+Les deux joueurs remplissent alternativement les cellules vides. 
+Au début du jeu, il y a 9 cellules vides dans un panneau 3x3. Le but est 
+de placer 3 signes identiques sur une colonne, une ligne ou une diagonale. 
+Le jeu prend fin si le panneau est entièrement rempli et si aucun joueur 
+n’arrive à atteindre le but. Bonne chance!
---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/boardcase'
+require_relative 'lib/players'
+require_relative 'lib/board'
+
+def presentation
+
+ puts "Bienvenue dans le célèbre jeu du morpion"
+ puts "nous allons maintenant presenter chaque joueur"
+ puts "=========================================="
+
+	puts "en premier nous acceuillons"
+	player1 = gets.chomp
+		
+	puts "et son challenger"
+	player2 = gets.chomp
+
+ puts "========================================="
+	
+end
+
+def
+end
+binding.pry
